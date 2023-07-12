d599fbc0e2734d413b71ddde04a418b1bf76d3b3
Niracha Marchetti <marchettibiw@gmail.com>
2023-07-12 13:13:01 +0200

    cleanup push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { uid } from "uid";
+import Form from "./components/Form";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [activities, setActivities] = useState([]);
+  const [weather, setWeather] = useState([]);
+
+  function addActivity(newActivity) {
+    setActivities([...activities, { id: uid(), ...newActivity }]);
+  }
+
+  useEffect(() => {
+    async function fetchWeather() {
+      try {
+        const response = await fetch(
+          "https://example-apis.vercel.app/api/weather"
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setWeather(data);
+        } else {
+          console.error("Bad response");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchWeather();
+  }, [setWeather]);
+
+  console.log(activities);
+
+  return <Form addActivity={addActivity} />;
 }
 
 export default App;
---
+++ src/components/Form/index.js
@@ -0,0 +1,38 @@
+export default function Form({ addActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formElements = form.elements;
+    const data = {
+      name: formElements.name.value,
+      isForGoodWeather: formElements.isForGoodWeather.checked,
+    };
+    addActivity(data);
+    form.reset();
+    formElements.name.focus();
+  }
+
+  return (
+    <div>
+      <form
+        className="form"
+        aria-labelledby="favourite-holiday"
+        onSubmit={handleSubmit}
+      >
+        <h1>This is the heading</h1>
+        <label htmlFor="activity">Add new activity: </label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          placeholder="e.g. name new activity"
+        />
+        <label htmlFor="isGoodWeather">Good-weather activity: </label>
+        <input id="checkbox" type="checkbox" name="isForGoodWeather" />
+        <button type="submit" className="form__submit-button">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
